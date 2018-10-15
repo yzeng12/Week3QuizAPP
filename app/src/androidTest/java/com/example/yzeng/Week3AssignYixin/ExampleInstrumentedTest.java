7f0cb862930045b73e56c250ec7372dfc31130de
yzeng12 <zengyx1995@gmail.com>
2018-10-15 12:51:47 -0500

    Added Tablet support

--- app/src/androidTest/java/com/example/yzeng/Week3AssignYixin/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/yzeng/Week3AssignYixin/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.cln62.week3hwquizapp", appContext.getPackageName());
+        assertEquals("com.example.yzeng.week3hwquizapp", appContext.getPackageName());
     }
 }
